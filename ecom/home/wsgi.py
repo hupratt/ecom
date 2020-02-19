0c9b675611867f1ba705a8fe47f9d3865efd3064
hupratt <hprattdo@gmail.com>
2020-02-19 16:09:21 +0100

    idem

--- ecom/home/wsgi.py
+++ ecom/home/wsgi.py
@@ -3,6 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.append("/home/ubuntu/Dev/ecom")
 sys.path.append("/home/ubuntu/Dev/ecom/ecom")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings")
 
 application = get_wsgi_application()
