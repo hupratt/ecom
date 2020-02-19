4ef751d4b73aae438bc3eba77471d7e2cb92faf0
hupratt <hprattdo@gmail.com>
2020-02-19 16:06:34 +0100

    debugging wsgi bug on apache

--- ecom/home/wsgi.py
+++ ecom/home/wsgi.py
@@ -1,8 +1,8 @@
 import os, sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append("/home/ubuntu/Dev/myshop")
-sys.path.append("/home/ubuntu/Dev/myshop/ecom")
+sys.path.append("/home/ubuntu/Dev/ecom")
+sys.path.append("/home/ubuntu/Dev/ecom/ecom")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings")
 
 application = get_wsgi_application()
