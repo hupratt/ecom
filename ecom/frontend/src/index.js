841d1d52811c513123b3e21e49334081769b0278
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-02-24 11:32:39 +0100

    doc for sw

--- ecom/frontend/src/index.js
+++ ecom/frontend/src/index.js
@@ -12,4 +12,9 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById("root"));
+
+/* Allows caching, push notifications and queues offline actions
+This action fires a seperate thread to install on
+When changed, the old service worker remains active if there are tabs open
+This means that in order to use the latest SW after a change you'll need to close the tab */
 registerServiceWorker();
