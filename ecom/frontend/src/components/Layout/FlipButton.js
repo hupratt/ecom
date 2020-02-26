213c5b6fbfd4769d88354f723949edf660f01939
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-02-26 10:46:31 +0100

    simplify component with refactoring and separation of concerns

--- ecom/frontend/src/components/Layout/FlipButton.js
+++ ecom/frontend/src/components/Layout/FlipButton.js
@@ -1,39 +1,26 @@
 import React from "react";
 
-class FlipButton extends React.Component {
-  state = {
-    flip: false,
-    className: "bk-bookdefault"
-  };
-  handleFlip = (flipState, isbn) => {
-    this.setState({
-      flip: !flipState,
-      className: flipState ? "bk-viewback" : "bk-bookdefault"
-    });
-    let book = document.getElementById(isbn);
-    if (flipState) {
-      book.classList.add("bk-bookdefault");
-      book.classList.remove("bk-viewback");
-    } else {
-      book.classList.remove("bk-bookdefault");
-      book.classList.add("bk-viewback");
-    }
-  };
-  render() {
-    let { flip, className } = this.state;
-    let { isbn } = this.props;
-
-    return (
-      <button
-        className={className}
-        onClick={() => {
-          this.handleFlip(flip, isbn);
-        }}
-      >
-        Flip
-      </button>
-    );
+const handleFlip = isbn => {
+  let book = document.getElementById(isbn);
+  let className = book.className;
+  if (className === "bk-book bk-viewback") {
+    book.classList.add("bk-bookdefault");
+    book.classList.remove("bk-viewback");
+  } else {
+    book.classList.remove("bk-bookdefault");
+    book.classList.add("bk-viewback");
   }
-}
+};
+const FlipButton = ({ isbn }) => {
+  return (
+    <button
+      onClick={() => {
+        handleFlip(isbn);
+      }}
+    >
+      Flip
+    </button>
+  );
+};
 
 export default FlipButton;
