55b8e74edc83928b23d0b7bdc691fe7a57e66e8f
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-02-26 11:06:52 +0100

    simplify component with refactoring and separation of concerns

--- ecom/frontend/src/components/Layout/ViewInsideButton.js
+++ ecom/frontend/src/components/Layout/ViewInsideButton.js
@@ -1,47 +1,35 @@
 import React from "react";
 
-class ViewInsideButton extends React.Component {
-  state = {
-    flip: false,
-    className: "bk-bookdefault"
-  };
-  handleFlip = (flipState, isbn) => {
-    this.setState({
-      flip: !flipState,
-      className: flipState ? "bk-viewinside" : "bk-bookdefault"
-    });
-    let book = document.getElementById(isbn);
-    let image = document.querySelector(`div[id='${isbn}'] img`);
-    let bookcoverinside = document.querySelector(".bk-cover-back");
-    if (flipState) {
-      book.classList.add("bk-bookdefault");
-      book.classList.remove("bk-viewinside");
-      image.classList.remove("whitebackground");
-      bookcoverinside.classList.remove("whitebackground");
-      book.classList.remove("whitebackground");
-    } else {
-      book.classList.remove("bk-bookdefault");
-      book.classList.add("bk-viewinside");
-      image.classList.add("whitebackground");
-      bookcoverinside.classList.add("whitebackground");
-      book.classList.add("whitebackground");
-    }
-  };
-  render() {
-    let { flip, className } = this.state;
-    let { isbn } = this.props;
-
-    return (
-      <button
-        className={className}
-        onClick={() => {
-          this.handleFlip(flip, isbn);
-        }}
-      >
-        View Inside
-      </button>
-    );
+const handleViewInside = isbn => {
+  let book = document.getElementById(isbn);
+  let className = book.className;
+  let image = document.querySelector(`div[id='${isbn}'] img`);
+  let bookcoverinside = document.querySelector(".bk-cover-back");
+  if (className === "bk-book bookdefault") {
+    book.classList.remove("bk-bookdefault");
+    book.classList.add("bk-viewinside");
+    image.classList.add("whitebackground");
+    bookcoverinside.classList.add("whitebackground");
+    book.classList.add("whitebackground");
+  } else {
+    book.classList.add("bk-bookdefault");
+    book.classList.remove("bk-viewinside");
+    image.classList.remove("whitebackground");
+    bookcoverinside.classList.remove("whitebackground");
+    book.classList.remove("whitebackground");
   }
-}
+};
+
+const ViewInsideButton = ({ isbn }) => {
+  return (
+    <button
+      onClick={() => {
+        handleViewInside(isbn);
+      }}
+    >
+      View Inside
+    </button>
+  );
+};
 
 export default ViewInsideButton;
