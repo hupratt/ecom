5556db4061f708192a2f8355afe578d8548b9eed
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-02-27 18:21:14 +0100

    🔨 Refactoring as it was getting unwieldy

--- ecom/frontend/src/components/Layout/TopNavigationWithAuth.js
+++ ecom/frontend/src/components/Layout/TopNavigationWithAuth.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Dropdown, Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import CartDropDownNoItems from "./CartDropDownNoItems"
+import CartDropDownWithItems from "./CartDropDownWithItems"
+import { withItemsInTheCart } from "../../hoc/hoc";
 
 const TopNavigationWithAuth = ({ cart, loading, logout }) => {
   return (
@@ -17,26 +20,7 @@ const TopNavigationWithAuth = ({ cart, loading, logout }) => {
             className="link item"
         >
         <Dropdown.Menu>
-        {cart !== null ? (
-            <React.Fragment>
-            {cart.order_items.map(order_item => {
-                return (
-                <Dropdown.Item key={order_item.id}>
-                    {order_item.quantity} x {order_item.livre.titre}
-                </Dropdown.Item>
-                );
-            })}
-            {cart.order_items.length < 1 ? (
-                <Dropdown.Item>No items in your cart</Dropdown.Item>
-            ) : null}
-            <Dropdown.Divider />
-            <Link to="/order-summary">
-                <Dropdown.Item icon="arrow right" text="Checkout"/>
-            </Link>
-            </React.Fragment>
-        ) : (
-            <Dropdown.Item>No items in your cart</Dropdown.Item>
-        )}
+        <CartDropDownWithItemsAndNoItems cart={cart}/>
         </Dropdown.Menu>
         </Dropdown>
         <Menu.Item header onClick={() => logout()}>
@@ -48,3 +32,5 @@ const TopNavigationWithAuth = ({ cart, loading, logout }) => {
 };
 
 export default TopNavigationWithAuth;
+
+const CartDropDownWithItemsAndNoItems = withItemsInTheCart(CartDropDownWithItems, CartDropDownNoItems);
