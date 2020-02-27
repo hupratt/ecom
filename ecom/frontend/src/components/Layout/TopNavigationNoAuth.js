e036c814bc6918600fc42dbb90c7a37751b8cee8
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-02-27 17:57:13 +0100

    Create TopNavigationNoAuth.js

---
+++ ecom/frontend/src/components/Layout/TopNavigationNoAuth.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const TopNavigationNoAuth = () => {
+  return (
+    <React.Fragment>
+        <Menu.Menu position="right">
+        <Link to="/login">
+        <Menu.Item header>Login</Menu.Item>
+        </Link>
+        <Link to="/signup">
+        <Menu.Item header>Signup</Menu.Item>
+        </Link>
+        </Menu.Menu>
+    </React.Fragment>
+  );
+};
+
+export default TopNavigationNoAuth;
