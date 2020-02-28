c524830bbde1cf52cbc1c6fb004d46948bca3442
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-02-28 13:03:17 +0100

    remove unused class

--- ecom/frontend/src/components/Payment/Checkout.js
+++ ecom/frontend/src/components/Payment/Checkout.js
@@ -79,41 +79,6 @@ const OrderPreview = props => {
   );
 };
 
-class CouponForm extends Component {
-  state = {
-    code: ""
-  };
-
-  handleChange = e => {
-    this.setState({
-      code: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
-    const { code } = this.state;
-    this.props.handleAddCoupon(e, code);
-    this.setState({ code: "" });
-  };
-
-  render() {
-    const { code } = this.state;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Field>
-          <label>Coupon code</label>
-          <input
-            placeholder="Enter a coupon.."
-            value={code}
-            onChange={this.handleChange}
-          />
-        </Form.Field>
-        <Button type="submit">Submit</Button>
-      </Form>
-    );
-  }
-}
-
 class CheckoutForm extends Component {
   state = {
     data: null,
