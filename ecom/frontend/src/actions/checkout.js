0a0ab581af169a752289ae5dce55a08067e55537
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-03-06 13:54:16 +0100

    🔨 refactoring for redux

---
+++ ecom/frontend/src/actions/checkout.js
@@ -0,0 +1,114 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+import { orderSummaryURL, addressListURL } from "../constants";
+
+export const shippingSuccess = (shippingAddresses, selectedShippingAddress) => {
+  return {
+    type: actionTypes.FETCH_SHIPPING_ADD,
+    shippingAddresses: shippingAddresses,
+    selectedShippingAddress: selectedShippingAddress,
+    loading: false
+  };
+};
+export const billingSuccess = (billingAddresses, selectedBillingAddress) => {
+  return {
+    type: actionTypes.FETCH_BILLING_ADD,
+    selectedShippingAddress: billingAddresses,
+    selectedBillingAddress: selectedBillingAddress,
+    loading: false
+  };
+};
+
+export const checkoutFail = error => {
+  return {
+    type: actionTypes.CART_FAIL,
+    error: error
+  };
+};
+
+export const start = () => {
+  return {
+    type: actionTypes.START
+  };
+};
+
+export const error = err => {
+  return {
+    type: actionTypes.ERROR,
+    error: err
+  };
+};
+
+handleGetDefaultAddress = addresses => {
+  const filteredAddresses = addresses.filter(el => el.default === true);
+  if (filteredAddresses.length > 0) {
+    return filteredAddresses[0].id;
+  }
+  return "";
+};
+
+export const handleFetchBillingAddresses = () => {
+  return dispatch => {
+    dispatch(start());
+    axios
+      .get(addressListURL("B"))
+      .then(res => {
+        const billingAddresses = res.data.map(a => {
+          return {
+            key: a.id,
+            text: `${a.street_address}, ${a.apartment_address}, ${a.country}`,
+            value: a.id
+          };
+        });
+        const selectedBillingAddress = handleGetDefaultAddress(res.data);
+        dispatch(billingSuccess(billingAddresses, selectedBillingAddress));
+      })
+      .catch(err => {
+        dispatch(error(err));
+      });
+  };
+};
+
+export const handleFetchShippingAddresses = () => {
+  return dispatch => {
+    dispatch(start());
+    axios
+      .get(addressListURL("S"))
+      .then(res => {
+        const shippingAddresses = res.data.map(a => {
+          return {
+            key: a.id,
+            text: `${a.street_address}, ${a.apartment_address}, ${a.country}`,
+            value: a.id
+          };
+        });
+        const selectedShippingAddress = handleGetDefaultAddress(res.data);
+        dispatch(shippingSuccess(shippingAddresses, selectedShippingAddress));
+      })
+      .catch(err => {
+        dispatch(error(err));
+      });
+  };
+};
+
+export const handleFetchOrder = history => {
+  return dispatch => {
+    dispatch(start());
+    axios
+      .get(orderSummaryURL)
+      .then(res => {
+        dispatch({
+          type: actionTypes.FETCH_ORDER,
+          data: res.data,
+          loading: false
+        });
+      })
+      .catch(err => {
+        if (err.response.status === 404) {
+          history.push("/products");
+        } else {
+          dispatch(error(err));
+        }
+      });
+  };
+};
