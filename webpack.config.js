058496320ccf9755342964acdb9489278ccfd67b
hupratt <hprattdo@gmail.com>
2020-03-22 19:00:54 +0100

    new webpack config (again)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   watch: true,
   entry: "./ecom/frontend/src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "ecom/frontend/static/frontend/main.js")
+    path: path.resolve(__dirname, "ecom/frontend/static/frontend")
   },
   module: {
     rules: [
@@ -40,5 +41,27 @@ module.exports = {
       }
     ]
   },
-  plugins: [new Dotenv({ path: "./.env.development" })]
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"],
+    extensions: ["*", ".js", ".scss"]
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dev"),
+    historyApiFallback: true,
+    hot: true
+  },
+  plugins: [
+    new Dotenv({ path: "./.env.development" }),
+    new HtmlWebpackPlugin({
+      template: path.join(
+        __dirname,
+        "ecom/frontend/templates/frontend/index.html"
+      ),
+      favicon: path.join(
+        __dirname,
+        "ecom/frontend/static/icons/logopetiteportugaise300.png"
+      )
+    })
+  ]
 };
