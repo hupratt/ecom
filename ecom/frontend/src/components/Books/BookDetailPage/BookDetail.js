a4956a382fae55c61c6858d41d452a0f876fd595
hupratt <hprattdo@gmail.com>
2020-04-10 12:21:19 +0200

    👌 Simpler UI as requested by susana

--- ecom/frontend/src/components/Books/BookDetailPage/BookDetail.js
+++ ecom/frontend/src/components/Books/BookDetailPage/BookDetail.js
@@ -27,9 +27,6 @@ const BookDetail = ({ handleAddToCart, book, isAuthenticated }) => {
                           src={mediaEndpoint + book.isbn + ".jpg"}
                           alt=""
                         />
-                        <div className="zoom-icon">
-                          <i className="fa fa-search-plus" />
-                        </div>
                       </div>
                     </div>
 
@@ -38,9 +35,6 @@ const BookDetail = ({ handleAddToCart, book, isAuthenticated }) => {
                         <div className="pd-title">
                           <span>{book.auteur_nom}</span>
                           <h3>{book.titre}</h3>
-                          <a href="#" className="heart-icon">
-                            <i className="far fa-heart" />
-                          </a>
                         </div>
                         <div className="pd-rating">
                           {[...Array(book.note)].map((e, i) => (
@@ -57,60 +51,34 @@ const BookDetail = ({ handleAddToCart, book, isAuthenticated }) => {
                               style={{ color: "#252525" }}
                             />
                           ))}
-
-                          <span>({book.note}) </span>
                         </div>
                         <div className="pd-desc">
                           <p>{book.description}</p>
                           <h4>
-                            {book.prix} €<span>629.99 €</span>
+                            {book.prix} €
+                            <span>
+                              {" "}
+                              {book.prix_barre} {book.prix_barre && "€"}
+                            </span>
                           </h4>
                         </div>
-                        <div className="pd-color">
-                          <h6>Color</h6>
-                          <div className="pd-color-choose">
-                            <div className="cc-item">
-                              <input type="radio" id="cc-black" />
-                              <label htmlFor="cc-black" />
-                            </div>
-                            <div className="cc-item">
-                              <input type="radio" id="cc-yellow" />
-                              <label
-                                htmlFor="cc-yellow"
-                                className="cc-yellow"
-                              />
-                            </div>
-                            <div className="cc-item">
-                              <input type="radio" id="cc-violet" />
-                              <label
-                                htmlFor="cc-violet"
-                                className="cc-violet"
-                              />
+                        {[
+                          ...Array((book.langue_nom || "none").split("/")),
+                        ].forEach((el) => (
+                          <div className="pd-lang-choose">
+                            <div className="sc-lang">
+                              <input type="radio" id="sm-size" />
+                              <label htmlFor="sm-lang">{el}</label>
                             </div>
                           </div>
-                        </div>
-                        <div className="pd-size-choose">
-                          <div className="sc-item">
+                        ))}
+                        {Array((book.langue_nom || "none").split("/")).forEach(
+                          (el) => (
                             <input type="radio" id="sm-size" />
-                            <label htmlFor="sm-size">s</label>
-                          </div>
-                          <div className="sc-item">
-                            <input type="radio" id="md-size" />
-                            <label htmlFor="md-size">m</label>
-                          </div>
-                          <div className="sc-item">
-                            <input type="radio" id="lg-size" />
-                            <label htmlFor="lg-size">l</label>
-                          </div>
-                          <div className="sc-item">
-                            <input type="radio" id="xl-size" />
-                            <label htmlFor="xl-size">xs</label>
-                          </div>
-                        </div>
+                          )
+                        )}
+
                         <div className="quantity">
-                          <div className="pro-qty">
-                            <input type="text" defaultValue={1} />
-                          </div>
                           <a
                             href="#"
                             className="primary-btn pd-cart"
@@ -129,233 +97,9 @@ const BookDetail = ({ handleAddToCart, book, isAuthenticated }) => {
                             <span>LANGUAGES</span>: {book.langue_nom}
                           </li>
                           <li>
-                            <span>QUANTITY</span>: {book.quantite}
+                            <span>QUANTITY</span>: {book.get_quantity}
                           </li>
                         </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="product-tab">
-                    <div className="tab-item">
-                      <ul className="nav" role="tablist">
-                        <li>
-                          <a
-                            className="active"
-                            data-toggle="tab"
-                            href="#tab-1"
-                            role="tab"
-                          >
-                            DESCRIPTION
-                          </a>
-                        </li>
-                        <li>
-                          <a data-toggle="tab" href="#tab-2" role="tab">
-                            SPECIFICATIONS
-                          </a>
-                        </li>
-                        <li>
-                          <a data-toggle="tab" href="#tab-3" role="tab">
-                            Customer Reviews (02)
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="tab-item-content">
-                      <div className="tab-content">
-                        <div
-                          className="tab-pane fade-in active"
-                          id="tab-1"
-                          role="tabpanel"
-                        >
-                          <div className="product-content">
-                            <div className="row">
-                              <div className="col-lg-7">
-                                <h5>Introduction</h5>
-                                <p>
-                                  Lorem ipsum dolor sit amet, consectetur
-                                  adipisicing elit, sed do eiusmod tempor
-                                  incididunt ut labore et dolore magna aliqua.
-                                  Ut enim ad minim veniam, quis nostrud
-                                  exercitation ullamco laboris nisi ut aliquip
-                                  ex ea commodo consequat. Duis aute irure dolor
-                                  in{" "}
-                                </p>
-                                <h5>Features</h5>
-                                <p>
-                                  Lorem ipsum dolor sit amet, consectetur
-                                  adipisicing elit, sed do eiusmod tempor
-                                  incididunt ut labore et dolore magna aliqua.
-                                  Ut enim ad minim veniam, quis nostrud
-                                  exercitation ullamco laboris nisi ut aliquip
-                                  ex ea commodo consequat. Duis aute irure dolor
-                                  in{" "}
-                                </p>
-                              </div>
-                              <div className="col-lg-5">
-                                <img
-                                  src="img/product-single/tab-desc.jpg"
-                                  alt=""
-                                />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div
-                          className="tab-pane fade"
-                          id="tab-2"
-                          role="tabpanel"
-                        >
-                          <div className="specification-table">
-                            <table>
-                              <tbody>
-                                <tr>
-                                  <td className="p-catagory">
-                                    Customer Rating
-                                  </td>
-                                  <td>
-                                    <div className="pd-rating">
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star" />
-                                      <i className="fa fa-star-o" />
-                                      <span>(5)</span>
-                                    </div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Price</td>
-                                  <td>
-                                    <div className="p-price">$495.00</div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Add To Cart</td>
-                                  <td>
-                                    <div className="cart-add">
-                                      + add to cart
-                                    </div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Availability</td>
-                                  <td>
-                                    <div className="p-stock">22 in stock</div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Weight</td>
-                                  <td>
-                                    <div className="p-weight">1,3kg</div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Size</td>
-                                  <td>
-                                    <div className="p-size">Xxl</div>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Color</td>
-                                  <td>
-                                    <span className="cs-color" />
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td className="p-catagory">Sku</td>
-                                  <td>
-                                    <div className="p-code">00012</div>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
-                        <div
-                          className="tab-pane fade"
-                          id="tab-3"
-                          role="tabpanel"
-                        >
-                          <div className="customer-review-option">
-                            <h4>2 Comments</h4>
-                            <div className="comment-option">
-                              <div className="co-item">
-                                <div className="avatar-pic">
-                                  <img
-                                    src="img/product-single/avatar-1.png"
-                                    alt=""
-                                  />
-                                </div>
-                                <div className="avatar-text">
-                                  <div className="at-rating">
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star-o" />
-                                  </div>
-                                  <h5>
-                                    Brandon Kelley <span>27 Aug 2019</span>
-                                  </h5>
-                                  <div className="at-reply">Nice !</div>
-                                </div>
-                              </div>
-                              <div className="co-item">
-                                <div className="avatar-pic">
-                                  <img
-                                    src="img/product-single/avatar-2.png"
-                                    alt=""
-                                  />
-                                </div>
-                                <div className="avatar-text">
-                                  <div className="at-rating">
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star" />
-                                    <i className="fa fa-star-o" />
-                                  </div>
-                                  <h5>
-                                    Roy Banks <span>27 Aug 2019</span>
-                                  </h5>
-                                  <div className="at-reply">Nice !</div>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="personal-rating">
-                              <h6>Your Ratind</h6>
-                              <div className="rating">
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star-o" />
-                              </div>
-                            </div>
-                            <div className="leave-comment">
-                              <h4>Leave A Comment</h4>
-                              <form action="#" className="comment-form">
-                                <div className="row">
-                                  <div className="col-lg-6">
-                                    <input type="text" placeholder="Name" />
-                                  </div>
-                                  <div className="col-lg-6">
-                                    <input type="text" placeholder="Email" />
-                                  </div>
-                                  <div className="col-lg-12">
-                                    <textarea
-                                      placeholder="Messages"
-                                      defaultValue={""}
-                                    />
-                                    <button type="submit" className="site-btn">
-                                      Send message
-                                    </button>
-                                  </div>
-                                </div>
-                              </form>
-                            </div>
-                          </div>
-                        </div>
                       </div>
                     </div>
                   </div>
