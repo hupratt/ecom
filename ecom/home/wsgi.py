3157447f0e010d985d393ceafa940b045ca88a3e
hupratt <hprattdo@gmail.com>
2020-04-12 11:24:57 +0200

    change to env file feed

--- ecom/home/wsgi.py
+++ ecom/home/wsgi.py
@@ -4,7 +4,9 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append("/home/ubuntu/Dev/ecom")
 sys.path.append("/home/ubuntu/Dev/ecom/ecom")
 
-dotenv.read_dotenv("/home/ubuntu/Dev/ecom/.env")
+dotenv.read_dotenv(
+    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env")
+)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings")
 
 application = get_wsgi_application()
