fe9544dadda7a36c9ed553066b50f84a7bf0561b
hupratt <hprattdo@gmail.com>
2020-04-12 14:38:08 +0200

    more build changes

--- customgitpush.sh
+++ customgitpush.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 aws ec2 stop-instances --instance-ids i-00c1e0195543743f4
+aws ec2 stop-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 80
 
 aws ec2 modify-instance-attribute --instance-id i-00c1e0195543743f4 --instance-type "{\"Value\": \"t2.medium\"}"
+aws ec2 modify-instance-attribute --instance-id i-0dd6cb48c6232ede8 --instance-type "{\"Value\": \"t2.medium\"}"
 
 sleep 1
 
 aws ec2 start-instances --instance-ids i-00c1e0195543743f4
+aws ec2 start-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 60
 
@@ -20,14 +23,17 @@ git push
 sleep 500
 
 aws ec2 stop-instances --instance-ids i-00c1e0195543743f4
+aws ec2 stop-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 80
 
 aws ec2 modify-instance-attribute --instance-id i-00c1e0195543743f4 --instance-type "{\"Value\": \"t2.micro\"}"
+aws ec2 modify-instance-attribute --instance-id i-0dd6cb48c6232ede8 --instance-type "{\"Value\": \"t2.micro\"}"
 
 sleep 1
 
 aws ec2 start-instances --instance-ids i-00c1e0195543743f4
+aws ec2 start-instances --instance-ids i-0dd6cb48c6232ede8
 
 sleep 30
 
