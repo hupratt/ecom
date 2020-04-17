cb3385951ebe75745338f004c53a8d219abdbb05
hupratt <hprattdo@gmail.com>
2020-04-18 00:03:37 +0200

    update page working , token missing

--- ecom/frontend/src/components/Books/BookDetailPage/BookUpdatePage.js
+++ ecom/frontend/src/components/Books/BookDetailPage/BookUpdatePage.js
@@ -1,172 +1,148 @@
 import React from "react";
-import { s3_base_url } from "../../../constants";
+import { endpoint } from "../../../constants";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchBook } from "../../../actions/book";
 
 class BookUpdate extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      updatedBook: {
-        auteur_nom: "",
-        isbn: "",
-        note: "",
-        titre: "",
-        prix: "",
-        langue_nom: "",
-        genre_nom: "",
-      },
-
-      genderOptions: ["Male", "Female", "Others"],
-      skillOptions: ["Programming", "Development", "Design", "Testing"],
-    };
-  }
+  state = { updatedBook: {} };
   componentDidMount() {
     console.log(this.props.isAuthenticated);
-    if (this.props.isAuthenticated == true) {
-      this.props.fetchBook(this.props.match.params.bookID);
-      console.log(this.props.book);
-    }
+    this.setState({
+      updatedBook: this.props.book,
+    });
   }
 
-  handleInput(e) {
+  handleInput = (e) => {
     let value = e.target.value;
     let name = e.target.name;
-    this.setState(
-      (prevState) => {
-        return {
-          newUser: {
-            ...prevState.newUser,
-            [name]: value,
-          },
-        };
-      },
-      () => console.log(this.state.newUser)
-    );
-  }
-  handleFormSubmit(e) {
+    this.setState((prevState) => {
+      return {
+        updatedBook: {
+          ...prevState.updatedBook,
+          [name]: value,
+        },
+      };
+    });
+  };
+  handleFormSubmit = (e) => {
     e.preventDefault();
-    let userData = this.state.newUser;
-
-    fetch("http://example.com", {
+    const bookData = this.state.updatedBook;
+    console.log(bookData);
+    fetch(`${endpoint}/books/${this.props.book.id}/update/`, {
       method: "POST",
-      body: JSON.stringify(userData),
+      body: JSON.stringify(bookData),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
     }).then((response) => {
       response.json().then((data) => {
-        console.log("Successful" + data);
+        console.log(response);
       });
     });
-  }
+  };
 
   render() {
-    const { book } = this.props;
+    const {
+      auteur_nom,
+      isbn,
+      note,
+      titre,
+      prix,
+      langue_nom,
+      genre_nom,
+      description,
+    } = this.state.updatedBook;
     return (
       <div>
-        {book && (
-          <section className="product-shop spad page-details">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="row">
-                    <div className="col-lg-6">
-                      <div className="product-pic-zoom">
-                        <img
-                          className="product-big-img"
-                          src={s3_base_url + book.isbn + ".jpg"}
-                          alt=""
+        <section className="product-shop spad page-details">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <div className="product-details">
+                      <form className="product-details">
+                        <Input
+                          name={"auteur_nom"}
+                          title={"Author"}
+                          type={"text"}
+                          value={auteur_nom}
+                          handleChange={this.handleInput}
+                          placeholder={auteur_nom}
                         />
-                      </div>
-                    </div>
+                        <Input
+                          name={"isbn"}
+                          title={"ISBN"}
+                          type={"text"}
+                          value={isbn}
+                          handleChange={this.handleInput}
+                          placeholder={isbn}
+                        />
+                        <Input
+                          name={"titre"}
+                          title={"Titre"}
+                          type={"text"}
+                          value={titre}
+                          handleChange={this.handleInput}
+                          placeholder={titre}
+                        />
+                        <Input
+                          name={"note"}
+                          title={"Note"}
+                          type={"text"}
+                          value={note}
+                          handleChange={this.handleInput}
+                          placeholder={note}
+                        />
+                        <Input
+                          name={"prix"}
+                          title={"Prix"}
+                          type={"text"}
+                          value={prix}
+                          handleChange={this.handleInput}
+                          placeholder={prix}
+                        />
+                        <Input
+                          name={"langue_nom"}
+                          title={"Langue"}
+                          type={"text"}
+                          value={langue_nom}
+                          handleChange={this.handleInput}
+                          placeholder={langue_nom}
+                        />
+                        <Input
+                          name={"genre_nom"}
+                          title={"Genre"}
+                          type={"text"}
+                          value={genre_nom}
+                          handleChange={this.handleInput}
+                          placeholder={genre_nom}
+                        />
+                        <TextArea
+                          name={"description"}
+                          title={"Description"}
+                          type={"text"}
+                          value={description}
+                          handleChange={this.handleInput}
+                          placeholder={description}
+                          rows={15}
+                          cols={1}
+                        />
 
-                    <div className="col-lg-6">
-                      <div className="product-details">
-                        <form
-                          className="product-details"
-                          onSubmit={this.handleFormSubmit}
-                        >
-                          <Input
-                            name={"auteur_nom"}
-                            title={"Author"}
-                            type={"text"}
-                            value={book.auteur_nom}
-                            handleChange={this.handleInput}
-                            placeholder={book.auteur_nom}
-                          />
-                          <Input
-                            name={"isbn"}
-                            title={"ISBN"}
-                            type={"text"}
-                            value={book.isbn}
-                            handleChange={this.handleInput}
-                            placeholder={book.isbn}
-                          />
-                          <Input
-                            name={"titre"}
-                            title={"Titre"}
-                            type={"text"}
-                            value={book.titre}
-                            handleChange={this.handleInput}
-                            placeholder={book.titre}
-                          />
-                          <Input
-                            name={"note"}
-                            title={"Note"}
-                            type={"text"}
-                            value={book.note}
-                            handleChange={this.handleInput}
-                            placeholder={book.note}
-                          />
-                          <Input
-                            name={"prix"}
-                            title={"Prix"}
-                            type={"text"}
-                            value={book.prix}
-                            handleChange={this.handleInput}
-                            placeholder={book.prix}
-                          />
-                          <Input
-                            name={"langue_nom"}
-                            title={"Langue"}
-                            type={"text"}
-                            value={book.langue_nom}
-                            handleChange={this.handleInput}
-                            placeholder={book.langue_nom}
-                          />
-                          <Input
-                            name={"genre_nom"}
-                            title={"Genre"}
-                            type={"text"}
-                            value={book.genre_nom}
-                            handleChange={this.handleInput}
-                            placeholder={book.genre_nom}
-                          />
-                          <TextArea
-                            name={"description"}
-                            title={"Description"}
-                            type={"text"}
-                            value={book.description}
-                            handleChange={this.handleInput}
-                            placeholder={book.description}
-                            rows={15}
-                            cols={1}
-                          />
-
-                          <button type="button">Submit</button>
-                        </form>
-                      </div>
+                        <button type="button" onClick={this.handleFormSubmit}>
+                          Submit
+                        </button>
+                      </form>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </section>
-        )}
+          </div>
+        </section>
+        )
       </div>
     );
   }
