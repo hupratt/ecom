4714a3fa147fa10c8dc6441a20f2e14303fa6ec8
hupratt <hprattdo@gmail.com>
2020-04-22 20:05:14 +0200

    flags working

--- ecom/frontend/src/components/Layout/TopNavigation/TopNavigationNoAuth.js
+++ ecom/frontend/src/components/Layout/TopNavigation/TopNavigationNoAuth.js
@@ -4,43 +4,78 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import ReactFlagsSelect from "react-flags-select";
 import "react-flags-select/css/react-flags-select.css";
+import { withTranslation } from "react-i18next";
 
-const TopNavigationNoAuth = ({ onSearchChange }) => {
-  const { i18n, t } = useTranslation();
+class TopNavigationNoAuth extends React.Component {
+  componentDidMount() {
+    var language = window.navigator.userLanguage || window.navigator.language;
+    switch (true) {
+      case language.toLowerCase().includes("pt"):
+        this.refs.flag.updateSelected("PT");
+        break;
+      case language.toLowerCase().includes("fr"):
+        this.refs.flag.updateSelected("FR");
+        break;
+      case language.toLowerCase().includes("en"):
+        this.refs.flag.updateSelected("GB");
+        break;
+      case language.toLowerCase().includes("de"):
+        this.refs.flag.updateSelected("DE");
+        break;
+      default:
+        this.props.i18n.changeLanguage("GB");
+    }
+  }
+  onSelectFlag = (countryCode) => {
+    switch (countryCode) {
+      case "PT":
+        this.props.i18n.changeLanguage("pt");
+        break;
+      case "FR":
+        this.props.i18n.changeLanguage("fr");
+        break;
+      case "EN":
+        this.props.i18n.changeLanguage("en");
+        break;
+      case "DE":
+        this.props.i18n.changeLanguage("de");
+        break;
+      default:
+        this.props.i18n.changeLanguage("en");
+    }
+  };
+  render() {
+    const pt = this.props.t("Portuguese");
+    const fr = this.props.t("French");
+    const de = this.props.t("German");
+    const en = this.props.t("English");
 
-  const changeLanguage = (lng) => i18n.changeLanguage(lng);
-  const pt = t("Portuguese");
-  const fr = t("French");
-  const de = t("German");
-  const en = t("English");
-  return (
-    <React.Fragment>
-      <SearchNav onSearchChange={onSearchChange} />
-      {/* <div className="col-lg-3 text-right col-md-3">
-        <ul className="nav-right">
-          <a onClick={() => changeLanguage("pt")}>PT</a>
-          <a onClick={() => changeLanguage("de")}> / DE</a>
-          <a onClick={() => changeLanguage("en")}> / EN</a>
-          <a onClick={() => changeLanguage("fr")}> / FR</a>
-        </ul>
-      </div> */}
-      <div className="col-lg-3 text-right col-md-3">
-        <ul className="nav-right">
-          <ReactFlagsSelect
-            countries={["PT", "EN", "FR", "DE"]}
-            customLabels={{
-              PT: pt,
-              GB: en,
-              FR: fr,
-              DE: de,
-            }}
-            placeholder="Select Language"
-            showSelectedLabel={false}
-          />
-        </ul>
-      </div>
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <SearchNav onSearchChange={this.props.onSearchChange} />
 
-export default TopNavigationNoAuth;
+        <div className="col-lg-3 text-right col-md-3">
+          <ul className="nav-right">
+            <ReactFlagsSelect
+              ref="flag"
+              countries={["PT", "GB", "FR", "DE"]}
+              onSelect={this.onSelectFlag}
+              customLabels={{
+                PT: pt,
+                GB: en,
+                FR: fr,
+                DE: de,
+              }}
+              placeholder=""
+              showSelectedLabel={false}
+            />
+          </ul>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const TopNavigationNoAuthWithTrans = withTranslation()(TopNavigationNoAuth);
+
+export default TopNavigationNoAuthWithTrans;
