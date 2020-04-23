aaed19fb52ffa9d9891c5cff46a145ae9572e5b7
hupratt <hprattdo@gmail.com>
2020-04-23 08:39:05 +0200

    instance takes ages to stop

--- scripts/bash/customgitpush.sh
+++ scripts/bash/customgitpush.sh
@@ -2,7 +2,7 @@
 aws ec2 stop-instances --instance-ids i-00c1e0195543743f4
 aws ec2 stop-instances --instance-ids i-0dd6cb48c6232ede8
 
-sleep 160
+sleep 360
 
 aws ec2 modify-instance-attribute --instance-id i-00c1e0195543743f4 --instance-type "{\"Value\": \"t2.medium\"}"
 aws ec2 modify-instance-attribute --instance-id i-0dd6cb48c6232ede8 --instance-type "{\"Value\": \"t2.medium\"}"
