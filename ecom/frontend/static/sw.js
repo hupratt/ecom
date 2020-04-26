e3c9cfc24f9b6e684988fad33382a6efd4485e65
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-04-26 18:21:24 +0200

    Create sw.js

---
+++ ecom/frontend/static/sw.js
@@ -0,0 +1,14 @@
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  console.log("fetch event", evt);
+});
+
+// install service worker
+self.addEventListener("install", (event) => {
+  console.log("service worker has been installed", event);
+});
+
+// activate service worker
+self.addEventListener("activate", (event) => {
+  console.log("service worker has been activated", event);
+});
