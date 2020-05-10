f87eaf6d328c2378232e881a43133431eaa4ab16
hupratt <hprattdo@gmail.com>
2020-05-10 18:07:58 +0200

    responsiveness

--- ecom/home/urls.py
+++ ecom/home/urls.py
@@ -10,11 +10,10 @@ from .views import index, ServiceWorkerView
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("accounts/", include("coreaccount.googleurls")),
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("rest-auth/", include("rest_auth.urls")),
-    path("rest-auth/", include("coreaccount.googleurls")),
-    path("rest-auth/", include("coreaccount.facebookurls")),
-    path("rest-auth/", include("coreaccount.githuburls")),
     path("admin/", admin.site.urls),
     path("api/", include("core.api.urls")),
     # The service worker cannot be in /static because its scope will be limited to /static.
