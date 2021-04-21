44fb09136539d34510b7dfd5f6ac8ec8f5c97c1a
hupratt <hpratt@gmail.com>
2021-04-21 20:27:43 +0200

    revert

--- ecom/home/settings.py
+++ ecom/home/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY_books")
 if os.environ.get("DJANGO_DEVELOPMENT") is not None:
     DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["shop.lapetiteportugaise.eu", "127.0.0.1", "localhost", "lapetiteportugaise.eu"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
