e44ff4a8e5281fa059354e9d76e34409a08987e5
hupratt <hprattdo@gmail.com>
2021-05-14 09:29:32 +0200

    live debug

--- ecom/core/models.py
+++ ecom/core/models.py
@@ -151,7 +151,9 @@ class Livre(models.Model):
             # )
             # Fix me: image.url is wrong for some reason, appends https twice
             image_name = image.image.name
-            path = f"{settings.AWS_S3_CUSTOM_DOMAIN}/{image_name}"
+            path = f"{settings.AWS_S3_CUSTOM_DOMAIN}/{image_name}".replace(
+                "https://https://", "https://"
+            )
             return path
         else:
             return f"{settings.AWS_S3_CUSTOM_DOMAIN}/resources/no-image-icon.jpg"
